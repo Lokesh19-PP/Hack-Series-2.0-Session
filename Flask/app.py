78a62030b540e8c31497f36aa920e43d9cc92660
Lokesh Pawar <lokeshpawar1910@gmail.com>
2026-04-16 18:27:21 +0530

    Learn Jsonification in Flask

--- Flask/app.py
+++ Flask/app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
+
 @app.route("/")
 def home():
-    return "Lets begin learning flask"
+    return jsonify({
+        "message":"Api is running"
+    })
+
+@app.route("/test")
+def test():
+    return jsonify({
+        "message":"The test is working."
+    })
+
 if __name__ == "__main__":
     app.run(debug=True)
